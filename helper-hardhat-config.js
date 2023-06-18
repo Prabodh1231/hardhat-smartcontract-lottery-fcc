256146b9519c545f54e32f6fe73a36563821c858
Prabodh <prabodh1231@outlook.com>
2023-06-18 13:14:12 +0530

    Add network

---
+++ helper-hardhat-config.js
@@ -0,0 +1,10 @@
+const networkConfig = {
+    31337: {
+        name: "localhost",
+    },
+    // Price Feed Address, values can be obtained at https://docs.chain.link/data-feeds/price-feeds/addresses
+    11155111: {
+        name: "sepolia",
+        vrfCordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+    },
+}
